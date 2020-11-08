077129ab5fba75ae6744dddc9b8c7e51e14f8b82
Egor-arkharov <egorarkharov@gmail.com>
2020-11-08 22:52:51 +0300

    Весь js

---
+++ source/js/form.js
@@ -0,0 +1,56 @@
+'use strict';
+
+(function () {
+
+  let MONTHS = 12;
+  let TAX = 0.13
+  let MAX_PAYMENT = 260000;
+
+let calc = function () {
+  let calc = document.querySelector('#calc').content;
+
+  let sum1Year = calc.querySelector('[for="1-year"] .calc__sum');
+  let sum2Year = calc.querySelector('[for="2-year"] .calc__sum');
+  let sum3Year = calc.querySelector('[for="3-year"] .calc__sum');
+  let sum4Year = calc.querySelector('[for="4-year"] .calc__sum');
+
+  let arr = [sum1Year, sum2Year, sum3Year, sum4Year];
+
+  let wage = document.querySelector('.wage__input');
+  let wageValue = wage.value;
+
+  let payment = Math.floor(wageValue * MONTHS * TAX);
+
+  let currentSum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (currentSum + payment >= MAX_PAYMENT) {
+      arr[i].innerHTML = (MAX_PAYMENT - currentSum).toLocaleString('ru') + ' рублей';
+    } else {
+      arr[i].innerHTML = payment.toLocaleString('ru') + ' рублей';
+    }
+
+    currentSum += payment;
+  }
+}
+
+let removeSums = function () {
+  let sums = document.querySelectorAll('.calc__sum');
+
+  for (let i = 0; i < sums.length; i++) {
+    let num = sums[i].innerHTML;
+
+    if (isNaN(parseInt(num[0])) || parseInt(num[0]) === 0) {
+      sums[i].parentElement.parentElement.remove();
+    }
+  }
+}
+
+window.form = calc;
+
+window.form = {
+  calc: calc,
+  removeSums: removeSums,
+};
+
+})();
